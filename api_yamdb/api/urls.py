e21e5161fa39177d96314d40e2e6538d0ecd21f6
Vitaly Gudkov <vorvorsky@yandex.ru>
2022-09-20 18:44:35 +0300

    stable base

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import (UserSignupOrTokenViewSet, EditForUserViewSet,
-                    AdminUserEditViewSet,)
+#from .views import ()
 
 router_v1 = DefaultRouter()
-router_v1.register('me', EditForUserViewSet, basename='edit-for-user')
-router_v1.register(r'auth/signup', UserSignupOrTokenViewSet,
-                   basename='register')
-router_v1.register(r'auth/token/', UserSignupOrTokenViewSet,
-                   basename='token')
-router_v1.register('users', AdminUserEditViewSet, basename='edit-for-admin')
-router_v1.register('users', AdminUserEditViewSet, basename='edit-for-admin')
+
 
 urlpatterns = [
     path('v1/', include(router_v1.urls), name='review-api'),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -6,78 +6,8 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
 
 from .permissions import IsAdmin
-from .serializers import (UserSignupSerializer, GetTokenSerializer,
-                          EditForUserSerializer, AdminUserEditSerializer,)
-from reviews.models import User
+#from .serializers import
+#from reviews.models import
 
 
-class UserSignupOrTokenViewSet(mixins.CreateModelMixin,
-                               viewsets.GenericViewSet,):
-    """Вьюсет используемый для регистрации пользователя и получения токена"""
-    permission_classes = (permissions.AllowAny,)
 
-    def get_serializer_class(self):
-        """Определяет необходимый сериализатор в зависимости
-        от содержания запроса"""
-        if self.request.data.get('email') and self.request.data.get('username'):
-            return UserSignupSerializer
-        else:
-            if (self.request.data.get('username')
-                    and self.request.data.get('confirmation_code')):
-                return GetTokenSerializer
-
-
-    def create(self, request, *args, **kwargs):
-        """Определяет порядок действий: создание нового пользователя или
-        проверка кода подтверждения и отправка токена пользователю"""
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        if 'email' and 'username' in serializer.validated_data:
-            self.perform_create(serializer)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        if 'username' and 'confirmation_code' in serializer.validated_data:
-            user = get_object_or_404(
-                User,
-                username=serializer.validated_data["username"]
-            )
-            if default_token_generator.check_token(
-                    user, serializer.validated_data["confirmation_code"]
-            ):
-                token = AccessToken.for_user(user)
-                return Response({'token': str(token)},
-                                status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def perform_create(self, serializer):
-        """Создает пользователя и отправляет ему письмо с
-        кодом подтверждения"""
-        serializer.save()
-        user = get_object_or_404(User, serializer.validated_data['username'])
-        confirmation_code = default_token_generator.make_token(user)
-        send_mail(
-            subject='Вы зарегистрировались на YaMDb',
-            message=(
-                f'Код подтверждения(confirmation_code): {confirmation_code}'
-                'Используйте его для получения своего токена'
-            ),
-            from_email=None,
-            recipient_list=[user.email],
-        )
-
-
-class EditForUserViewSet(mixins.RetrieveModelMixin,
-                         mixins.UpdateModelMixin,
-                         viewsets.GenericViewSet,):
-    """Вьюсет для получения и изменения информации о себе"""
-    serializer_class = EditForUserSerializer
-
-    def get_object(self):
-        return self.request.user
-
-
-class AdminUserEditViewSet(viewsets.ModelViewSet):
-    """Вьюсет для получения списка пользователей, их регистрации
-    и редактирования"""
-    queryset = User.objects.all()
-    serializer_class = AdminUserEditSerializer
-    permission_classes = [IsAdmin, ]
