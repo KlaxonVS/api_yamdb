17ee5b93de70631ef381d9165dd80e2b12e1c9a7
Михаил Манузин <klinf157@gmail.com>
2022-09-21 18:58:26 +0300

    added IsAdminOrAuthorOrReadOnly permission class

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,3 +7,22 @@ class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_admin
+
+
+class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Позволяет редактировать объект только админинстратору, модератору и автору
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return (request.user.is_moderator or request.user.is_admin)
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
+
