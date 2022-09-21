d082f48d3a94bbfe5649cc7fca90e4c9974c7011
Vitaly Gudkov <vorvorsky@yandex.ru>
2022-09-21 17:29:57 +0300

    added description to forbidden_username_check

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -2,5 +2,6 @@ from django.core.exceptions import ValidationError
 
 
 def forbidden_username_check(name):
+    """Делает невозможным использовать <<me>> как имя"""
     if name == 'me':
         raise ValidationError('Нельзя использовать "me" как username')
