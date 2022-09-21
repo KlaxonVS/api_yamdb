2706d11cd265f2af0079e4a3021b6236ca6e4385
Vitaly Gudkov <vorvorsky@yandex.ru>
2022-09-21 17:23:55 +0300

    chenges to auth user related serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,3 +2,60 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
 from reviews.models import User
+
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    """Сериализатор для регистрации нового пользователя"""
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
+    username = serializers.CharField(
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
+
+    class Meta:
+        fields = ('email', 'username')
+        model = User
+
+
+class GetTokenSerializer(serializers.ModelSerializer):
+    """Сериализатор для получения токена"""
+    username = serializers.CharField()
+    confirmation_code = serializers.CharField()
+
+    class Meta:
+        fields = ('username', 'confirmation_code')
+        model = User
+
+
+class EditForUserSerializer(serializers.ModelSerializer):
+    """Сериализатор для получения пользователем информации о себе
+    и её редактирования"""
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
+    username = serializers.CharField(
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
+
+    class Meta:
+        fields = ('username', 'email', 'first_name',
+                  'last_name', 'bio', 'role')
+        model = User
+        read_only_fields = ('role',)
+
+
+class AdminUserEditSerializer(serializers.ModelSerializer):
+    """Сериализатор для получения администратором информации о пользователях
+        и её редактирования"""
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
+    username = serializers.CharField(
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
+
+    class Meta:
+        fields = ('username', 'email', 'first_name',
+                  'last_name', 'bio', 'role')
+        model = User
