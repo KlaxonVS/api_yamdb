f2acd54b09dd2ce5a3615d057ea871a781cc13b0
Klinf <klinf157@gmail.com>
2022-09-22 10:12:45 +0300

    Дополнил IsAdminOrAuthorOrReadOnly permission запретом на пост-запросы от неаутентифицированных пользователей

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -11,12 +11,16 @@ class IsAdmin(permissions.BasePermission):
 
 class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
     """
-    Позволяет редактировать объект только админинстратору, модератору и автору
+    Позволяет редактировать объект только админинстратору, модератору и автору.
+    Запрещает отправлять POST-запросы не аутентифицированным пользователям.
     """
 
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
+
+        if request.method == 'POST':
+            return bool(request.user and request.user.is_authenticated)
 
         return (request.user.is_moderator or request.user.is_admin)
 
