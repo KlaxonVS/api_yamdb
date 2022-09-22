83587a29342622872693fe69603fdaffd71470c3
Klinf <klinf157@gmail.com>
2022-09-22 14:18:34 +0300

    done Review and Comments endpoints

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import register_or_confirm_code, get_token, AdminUserEditViewSet
+from .views import (register_or_confirm_code, get_token,
+                    AdminUserEditViewSet, ReviewViewSet, CommentsViewSet)
 
 
 router_v1 = DefaultRouter()
 router_v1.register('users', AdminUserEditViewSet, basename='users')
+router_v1.register(
+    'titles/<int:title_id>/reviews/<int:review_id>/comments/',
+    CommentsViewSet,
+    basename='comments'
+)
+router_v1.register(
+    'titles/<int:title_id>/reviews/',
+    ReviewViewSet,
+    basename='reviews'
+)
+
 
 urlpatterns = [
     path('v1/', include(router_v1.urls), name='review-api'),
