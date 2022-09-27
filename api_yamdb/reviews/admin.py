2a19b7e65929141ad508ed5a07aeffa63af19e3c
Vitaly Gudkov <vorvorsky@yandex.ru>
2022-09-27 14:39:03 +0300

    added UserAdmin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,6 +3,26 @@ from django.contrib import admin
 from .models import User, Category, Genre, GenreTitle, Title, Comments, Review
 
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'email',
+        'username',
+        'full_name',
+        'role',
+    )
+    list_editable = ('role',)
+    search_fields = ('email', 'username',)
+    list_filter = ('role',)
+    empty_value_display = '-пусто-'
+
+    def full_name(self, obj,):
+        if not obj.get_full_name():
+            return 'Безимянный'
+        return obj.get_full_name()
+
+    full_name.short_description = 'Полное имя'
+
+
 class GenreInline(admin.TabularInline):
     model = GenreTitle
     extra = 2
@@ -23,7 +43,7 @@ class TitleAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Title, TitleAdmin)
 admin.site.register(Category)
 admin.site.register(Genre)
